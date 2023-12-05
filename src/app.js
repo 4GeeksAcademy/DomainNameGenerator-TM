c5886bc85479f634f2f1743f5d8437b1948f5730
tomasmj503 <86091325+tomasmj503@users.noreply.github.com>
2023-12-05 02:17:01 +0000

    domain

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net"];
+
+  function domain() {
+    let domains = [];
+    for (let i = 0; i < pronoun.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let h = 0; h < noun.length; h++) {
+          for (let k = 0; k < extension.length; k++) {
+            domains.push(
+              `<li> ${pronoun[i] + adj[j] + noun[h] + extension[k]} </li> <br>`
+            );
+          }
+        }
+      }
+    }
+    return domains;
+  }
+
+  document.querySelector("#domain").innerHTML = domain();
 };
